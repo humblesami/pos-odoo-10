12574b81385b24371a478bce4c34cafdc6f0864a
humblesami <Visio@123>
2020-04-27 17:09:26 +0500

    mark only own attendance

---
+++ attendance_mine/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Attendance Restriction",
+
+    'summary': """
+        An employee can mark only his/her attendance
+        """,
+
+
+    'author': "Visiomate sami",
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['hr_attendance'],
+}
---
+++ attendance_mine/models/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class HrAttendance(models.Model):
+    _inherit = 'hr.attendance'
+
+    @api.model
+    def create(self, vals):
+        me_emp = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
+        if not me_emp:
+            raise ValidationError('You must be an employee to mark attendance')
+        my_id = me_emp[0].id
+        if vals.get('employee_id') != my_id:
+            raise ValidationError('You can mark only your own attendance')
+        res = super(HrAttendance, self).create(vals)
+        return res
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
